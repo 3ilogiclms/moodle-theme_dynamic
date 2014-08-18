4217ddc05d66fd7d2ce3c64efec4ff1605b14faf
3i Logic LMS <lms@3ilogic.com>
2014-08-18 09:41:09 +0500

    Update lib.php

--- lib.php
+++ lib.php
@@ -30,21 +30,87 @@
  * @return string The processed CSS
  */
 function dynamic_process_css($css, $theme) {
- 
+
     if (!empty($theme->settings->regionwidth)) {
         $regionwidth = $theme->settings->regionwidth;
     } else {
         $regionwidth = null;
     }
     $css = dynamic_set_regionwidth($css, $regionwidth);
- 
+
     if (!empty($theme->settings->customcss)) {
         $customcss = $theme->settings->customcss;
     } else {
         $customcss = null;
     }
     $css = dynamic_set_customcss($css, $customcss);
- 
+
+    // Set the link color
+    if (!empty($theme->settings->linkcolor)) {
+        $linkcolor = $theme->settings->linkcolor;
+    } else {
+        $linkcolor = null;
+    }
+    $css = dynamic_set_linkcolor($css, $linkcolor);
+
+
+    // Set the blockheadercolor
+    if (!empty($theme->settings->blockheadercolor)) {
+        $blockheadercolor = $theme->settings->blockheadercolor;
+    } else {
+        $blockheadercolor = null;
+    }
+    $css = dynamic_set_blockheadercolor($css, $blockheadercolor);
+
+    // Set the blockheaderbg
+    if (!empty($theme->settings->blockheaderbg)) {
+        $blockheaderbg = $theme->settings->blockheaderbg;
+    } else {
+        $blockheaderbg = null;
+    }
+    $css = dynamic_set_blockheaderbg($css, $blockheaderbg);
+
+    // Set the menubg
+    if (!empty($theme->settings->menubg)) {
+        $menubg = $theme->settings->menubg;
+    } else {
+        $menubg = null;
+    }
+    $css = dynamic_set_menubg($css, $menubg);
+
+    // Set the menubg
+    if (!empty($theme->settings->menulinkhover)) {
+        $menulinkhover = $theme->settings->menulinkhover;
+    } else {
+        $menulinkhover = null;
+    }
+    $css = dynamic_set_menulinkhover($css, $menulinkhover);
+
+    // Set the menubg
+    if (!empty($theme->settings->menucolor)) {
+        $menucolor = $theme->settings->menucolor;
+    } else {
+        $menucolor = null;
+    }
+    $css = dynamic_set_menucolor($css, $menucolor);
+
+
+    // Set the menucolorhover
+    if (!empty($theme->settings->menucolorhover)) {
+        $menucolorhover = $theme->settings->menucolorhover;
+    } else {
+        $menucolorhover = null;
+    }
+    $css = dynamic_set_menucolorhover($css, $menucolorhover);
+
+    // Set the menucolorhover
+    if (!empty($theme->settings->footerbg)) {
+        $footerbg = $theme->settings->footerbg;
+    } else {
+        $footerbg = null;
+    }
+    $css = dynamic_set_footerbg($css, $footerbg);
+
     return $css;
 }
 
@@ -64,10 +130,10 @@ function dynamic_set_regionwidth($css, $regionwidth) {
     if (is_null($replacement)) {
         $replacement = 240;
     }
-    $css = str_replace($tag, $replacement.'px', $css);
-    $css = str_replace($doubletag, ($replacement*2).'px', $css);
-    $css = str_replace($rightmargintag, ($replacement*3-5).'px', $css);
-    $css = str_replace($leftmargintag, ($replacement+5).'px', $css);
+    $css = str_replace($tag, $replacement . 'px', $css);
+    $css = str_replace($doubletag, ($replacement * 2) . 'px', $css);
+    $css = str_replace($rightmargintag, ($replacement * 3 - 5) . 'px', $css);
+    $css = str_replace($leftmargintag, ($replacement + 5) . 'px', $css);
     return $css;
 }
 
@@ -98,7 +164,7 @@ function dynamic_set_customcss($css, $customcss) {
  */
 function dynamic_initialise_colourswitcher(moodle_page $page) {
     user_preference_allow_ajax_update('theme_dynamic_chosen_colour', PARAM_ALPHA);
-    $page->requires->yui_module('moodle-theme_dynamic-colourswitcher', 'M.theme_dynamic.initColourSwitcher', array(array('div'=>'#colourswitcher')));
+    $page->requires->yui_module('moodle-theme_dynamic-colourswitcher', 'M.theme_dynamic.initColourSwitcher', array(array('div' => '#colourswitcher')));
 }
 
 /**
@@ -107,7 +173,7 @@ function dynamic_initialise_colourswitcher(moodle_page $page) {
  * @param string $default The default colour to use, normally red
  * @return string The colour the user has selected
  */
-function dynamic_get_colour($default='red') {
+function dynamic_get_colour($default = 'red') {
     return get_user_preferences('theme_dynamic_chosen_colour', $default);
 }
 
@@ -120,8 +186,120 @@ function dynamic_get_colour($default='red') {
  */
 function dynamic_check_colourswitch() {
     $changecolour = optional_param('dynamiccolour', null, PARAM_ALPHA);
-    if (in_array($changecolour, array('red','green','blue','orange'))) {
+    if (in_array($changecolour, array('red', 'green', 'blue', 'orange'))) {
         return set_user_preference('theme_dynamic_chosen_colour', $changecolour);
     }
     return false;
+}
+
+/**
+ * Sets the link color variable in CSS
+ *
+ */
+function dynamic_set_linkcolor($css, $linkcolor) {
+    $tag = '[[setting:linkcolor]]';
+    $replacement = $linkcolor;
+    if (is_null($replacement)) {
+        $replacement = '#2d83d5';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the link color variable in CSS
+ *
+ */
+function dynamic_set_footerbg($css, $footerbg) {
+    $tag = '[[setting:footerbg]]';
+    $replacement = $footerbg;
+    if (is_null($replacement)) {
+        $replacement = '#5b0f01';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the font color of block header variable in CSS
+ *
+ */
+function dynamic_set_blockheadercolor($css, $blockheadercolor) {
+    $tag = '[[setting:blockheadercolor]]';
+    $replacement = $blockheadercolor;
+    if (is_null($replacement)) {
+        $replacement = '#32529a';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the font color of block header variable in CSS
+ *
+ */
+function dynamic_set_blockheaderbg($css, $blockheaderbg) {
+    $tag = '[[setting:blockheaderbg]]';
+    $replacement = $blockheaderbg;
+    if (is_null($replacement)) {
+        $replacement = '#32529a';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the font color of block header variable in CSS
+ *
+ */
+function dynamic_set_menubg($css, $menubg) {
+    $tag = '[[setting:menubg]]';
+    $replacement = $menubg;
+    if (is_null($replacement)) {
+        $replacement = '#000000';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the Background color of Menu Link Hover variable in CSS
+ *
+ */
+function dynamic_set_menulinkhover($css, $menulinkhover) {
+    $tag = '[[setting:menulinkhover]]';
+    $replacement = $menulinkhover;
+    if (is_null($replacement)) {
+        $replacement = '#5b0f01';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the font color of Menu Link variable in CSS
+ *
+ */
+function dynamic_set_menucolor($css, $menucolor) {
+    $tag = '[[setting:menucolor]]';
+    $replacement = $menucolor;
+    if (is_null($replacement)) {
+        $replacement = '#ffffff';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the font color of Menu Link variable in CSS
+ *
+ */
+function dynamic_set_menucolorhover($css, $menucolorhover) {
+    $tag = '[[setting:menucolorhover]]';
+    $replacement = $menucolor;
+    if (is_null($replacement)) {
+        $replacement = '#000000';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
 }
